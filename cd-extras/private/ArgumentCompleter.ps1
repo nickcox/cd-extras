957b9a7859a8abad18b9823eb96b1526481bafce
Nick Cox <ncox@live.com>
2018-02-12 08:34:07 +1100

    Completion to relative paths (#3).

--- cd-extras/private/ArgumentCompleter.ps1
+++ cd-extras/private/ArgumentCompleter.ps1
@@ -3,9 +3,11 @@ function RegisterArgumentCompleter([array]$commands) {
   Register-ArgumentCompleter -CommandName $commands -ParameterName Path -ScriptBlock {
     param($commandName, $parameterName, $wordToComplete, $commandAst, $boundParameters)
 
+    $currentDir = New-Object Uri ($pwd)
+
     $dirs = Expand-Path $wordToComplete $cde.CD_PATH |
       Where-Object {$_ -is [System.IO.DirectoryInfo]} |
-      % { $_.Fullname } |
+      % { if ($currentDir.IsBaseOf((New-Object Uri ($_)))) { Resolve-Path -Relative $_} else {$_} } |
       % { if ($_ -match ' ') { "'$_'" } else { $_ } } # quote if contains spaces
 
     $dirs | % {
