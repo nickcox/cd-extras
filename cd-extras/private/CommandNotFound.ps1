cf200745fc76fa6204143e068c99b89eafe1933b
Nick <nickcox1@gmail.com>
2020-02-09 15:21:26 +1100

    Don't invoke auto_cd unless at least one alphanumeric is given.

--- cd-extras/private/CommandNotFound.ps1
+++ cd-extras/private/CommandNotFound.ps1
@@ -4,13 +4,15 @@ function CommandNotFound($actions, $isUnderTest) {
     param($CommandName, $CommandLookupEventArgs)
 
     if ($CommandName -like 'get-*') { return }
-    
+
     # don't run as part of pipeline
     if ($MyInvocation.Line -match "$([regex]::Escape($CommandName))\s*\|") { return }
 
+    # don't run if no word characters given
+    if ($MyInvocation.Line -notmatch '\w') { return }
+
     # don't run unless invoked interactively
-    if ($CommandLookupEventArgs.CommandOrigin -ne 'Runspace' -and
-    !(&$isUnderTest)) { return }
+    if ($CommandLookupEventArgs.CommandOrigin -ne 'Runspace' -and !(&$isUnderTest)) { return }
 
     $actions | % { &$_ $CommandName $CommandLookupEventArgs }
   }.GetNewClosure()
